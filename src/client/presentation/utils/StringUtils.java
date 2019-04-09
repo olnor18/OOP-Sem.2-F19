8513c8c32081a1e41510ce3adb0d9cf12e83a6a2
olnor18 <olnor18@student.sdu.dk>
2019-04-09 19:01:50 +0200

    StringUtil started

---
+++ src/client/presentation/utils/StringUtils.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client.presentation.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Oliver
+ */
+public class StringUtils
+{
+   //Jeg er i gang her - Oliver
+    /* public static getBoldString(String s)
+    {
+        List<Character> list = s.chars().mapToObj(c -> ((char) c + 120205)).collect(Collectors.toList());
+        return (char)120205 + s;
+    }*/ 
+}
