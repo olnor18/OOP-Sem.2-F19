7647f421b543c085da73cf6ed9faa058d4d3137f
Frederik Hounsvad <Frederik@hounsvad.dk>
2019-04-17 15:46:46 +0200

    Added container classes

---
+++ src/client/presentation/modules/diary/MedicinalEntry.java
@@ -0,0 +1,12 @@
+/*This is code written by Frederik Alexander Hounsvad
+ * The use of this code in a non commercial and non exam environment is permitted
+ */
+package client.presentation.modules.diary;
+
+/**
+ *
+ * @author Hounsvad
+ */
+public class MedicinalEntry {
+
+}
