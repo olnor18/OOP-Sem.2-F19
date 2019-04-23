23942374dce4ce6974693aad7ef11dbed70065b6
Oliver Nordestgaard <olnor18@student.sdu.dk>
2019-04-23 08:20:14 +0200

    fixed some text

--- src/client/presentation/modules/dashboard/DashboardFXMLController.java
+++ src/client/presentation/modules/dashboard/DashboardFXMLController.java
@@ -61,34 +61,41 @@ public class DashboardFXMLController implements Initializable {
             new ActivityEntry(("Delete"), new Date(), "Everything"),
             new ActivityEntry(("Create journal"), new Date(), "Frederik Hounsvwad awdawdawdad")
         };
-        
+
         MessageEntry[] messageEntries = new MessageEntry[]{
-            new MessageEntry(("Ny bruger"), "John Lennon", "Vi har fået en ny bruger som hedder Gorm, så tag godt imod ham. Han er ny i afdelingen og kommer fremad til at arbejde med sildefilliteringen", new Date()),
+            new MessageEntry(("Ny bruger"), "John Lennon", "Vi har fået en ny bruger som hedder Gorm, så tag godt imod ham. Han er ny i afdelingen og kommer fremad til at være UX", new Date()),
             new MessageEntry(("Login"), "john", "Common login", new Date()),
             new MessageEntry(("Login"), "john", "Common login", new Date()),
             new MessageEntry(("Login"), "john", "Common login", new Date()),
             new MessageEntry(("Login"), "john", "Common login", new Date()),
             new MessageEntry(("Login"), "john", "Common login", new Date()),
-            new MessageEntry(("Login"), "john", "Common login", new Date()),
-        };
+            new MessageEntry(("Login"), "john", "Common login", new Date()),};
 
         activityView.getItems().addAll(activityEntries);
         activityView.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
             @Override
             public void handle(MouseEvent event) {
-                activityView.getSelectionModel().getSelectedItem().showPopup();
+                try {
+                    activityView.getSelectionModel().getSelectedItem().showPopup();
+                } catch (NullPointerException e) {
+
+                }
             }
         });
-        
+
         messageView.getItems().addAll(messageEntries);
         messageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
             @Override
             public void handle(MouseEvent event) {
-                messageView.getSelectionModel().getSelectedItem().showPopup();
+                try {
+                    messageView.getSelectionModel().getSelectedItem().showPopup();
+                } catch (NullPointerException e) {
+                }
             }
-        });
+        }
+        );
 
     }
 
